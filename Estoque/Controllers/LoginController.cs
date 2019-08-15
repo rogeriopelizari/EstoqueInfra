07be1ed0d9254a57fd6d736069e227c302f30209
rogerio.pelizari <rogerio.pelizari@navita.com.br>
2019-08-15 17:02:43 -0300

    Ajustes Novo Projeto

--- Estoque/Controllers/LoginController.cs
+++ Estoque/Controllers/LoginController.cs
@@ -1,11 +1,7 @@
 ﻿using Estoque.Models;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
 
 namespace Estoque.Controllers
 {
---
+++ Estoque/Scripts/navbar.js
@@ -0,0 +1,4 @@
+﻿// SideNav Initialization
+$(".button-collapse").sideNav();
+
+new WOW().init();
